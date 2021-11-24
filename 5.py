ea34c46822c989e98d93ab5b5a2947d4705d929b
j8 <walbhota@gmail.com>
2021-11-24 01:46:25 +0100

    codes to do addition using python

---
+++ 5.py
@@ -0,0 +1,13 @@
+# def add_two_nums(num1, num2):
+#   print(num1 + num2)
+
+# add_two_nums(4, 5)
+
+
+def add_two_nums(num1, num2):
+    return num1 + num2
+
+
+my_added_num = add_two_nums(4, 5)
+
+print(add_two_nums(4, 5))
